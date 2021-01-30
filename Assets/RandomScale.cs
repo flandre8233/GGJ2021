ff885893cc78a1281dc2c5ca1981beb561476825
Gibson's macbook2019 <>
2021-01-30 16:38:25 +0800

    加入爆炸特效 物件不再飛走 會重複飛行

---
+++ Assets/RandomScale.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomScale : MonoBehaviour
+{
+    void Start()
+    {
+        float RandomSize = Random.Range(1, 4);
+        transform.localScale = new Vector3(RandomSize, RandomSize, RandomSize);
+    }
+
+}
---
+++ Assets/ThrowCollision.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowCollision : MonoBehaviour
+{
+   void OnTriggerEnter2D(Collider2D other)
+   {
+       GameObject ExplosionPrefab = Resources.Load<GameObject>("Explosion");
+       Instantiate(ExplosionPrefab , gameObject.transform.position,Quaternion.Euler(0,0,Random.Range(0,360f)));
+   }
+}
