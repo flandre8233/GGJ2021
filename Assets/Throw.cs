3bc07a8ab3a48030e80a8e3a4139b78b84e976c0
Gibson's macbook2019 <>
2021-01-30 17:16:15 +0800

    no message

--- Assets/Throw.cs
+++ Assets/Throw.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 
 public class Throw : MonoBehaviour
 {
+    // 0 : white
+    // 1 : Red 
+    // 2 : blue 
+    public int Belong;
+
     Vector3 OrlDir;
+    float Speed;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,17 +19,30 @@ public class Throw : MonoBehaviour
         transform.parent = null;
         gameObject.AddComponent<ThrowMapLimit>();
         gameObject.AddComponent<ThrowCollision>();
+        Speed = 5;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(new Vector3(0, 0, 540 * Time.deltaTime));
-        transform.position += OrlDir * Time.deltaTime * 5;
+        transform.Rotate(new Vector3(0, 0, Speed*60 * Time.deltaTime));
+        transform.position += OrlDir * Time.deltaTime * Speed;
     }
 
-    private void OnDestroy() {
-        Destroy( GetComponent<ThrowMapLimit>() );
-        Destroy( GetComponent<ThrowCollision>() );
+    private void OnDestroy()
+    {
+        Destroy(GetComponent<ThrowMapLimit>());
+        Destroy(GetComponent<ThrowCollision>());
+    }
+
+    public void ReSetThrowing()
+    {
+        Speed = Random.Range(1f, 7.5f);
+        SetRandomOrlDir();
+    }
+
+     void SetRandomOrlDir()
+    {
+        OrlDir = Random.insideUnitCircle.normalized;
     }
 }
